7bb8cc6da2269d10f3210f5465a3e04f6f58bc96
David-Koshkadze <rootus3rr@pm.me>
2024-03-09 21:49:21 +0400

    part 2 d1

--- main.go
+++ main.go
@@ -32,6 +32,46 @@ func getNumbers(str string) int {
 	return num
 }
 
+// day 1 part 2
+
+var numberWords = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func getNumbersNumeric(str string) int {
+	var numStr string
+
+	words := strings.Fields(str)
+
+	for _, word := range words {
+		if num, ok := numberWords[word]; ok {
+			numStr += strconv.Itoa(num)
+			fmt.Println(num)
+		} else {
+			for _, char := range word {
+				if unicode.IsDigit(char) {
+					numStr += string(char)
+				}
+			}
+		}
+	}
+
+	if len(numStr) == 0 {
+		return 0
+	}
+
+	num, _ := strconv.Atoi(string(numStr[0]) + string(numStr[len(numStr)-1]))
+	return num
+}
+
 func main() {
 	file, err := os.Open("text_input.txt")
 
@@ -49,7 +89,9 @@ func main() {
 	for scanner.Scan() {
 		scannerTxt := scanner.Text()
 
-		number := getNumbers(scannerTxt)
+		number := getNumbersNumeric(scannerTxt)
+
+		// fmt.Println(number)
 
 		sum += number
 	}
@@ -58,5 +100,7 @@ func main() {
 		fmt.Println("Error:", err)
 	}
 
-	fmt.Println(sum)
+	// fmt.Println(sum)
+
+	fmt.Println(getNumbersNumeric("twonrpvnnmvkh2threejzcpz"))
 }
